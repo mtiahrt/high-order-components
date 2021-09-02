bed6bef9b0aa174aa95071e87b8139935e92ae45
mtiahrt <mark.tiahrt@outlook.com>
2021-09-02 11:14:55 -0600

    added basic function to manual form

--- src/Components/ManualLogEntry.js
+++ src/Components/ManualLogEntry.js
@@ -1,13 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const ManualLogEntry = () => {
+    const [hours, setHours] = useState(0)
+    const [activity, setActivity] = useState('');
+    const handleChange = ( e ) => {
+        if(e.target.classList.contains('activity')){
+            setActivity(e.target.value)
+            return
+        }
+        //make sure not letters are entered
+        if(/^\d*\.?\d*$/.test(e.target.value)){
+            setHours(e.target.value)
+        }
+
+    }
+
+
+    const addHealthStatManual = () => {
+
+    }
     return (
         <div className='manual-log'>
             <label for="activity">Enter Activity</label>
-            <input type="text" name="activity"/><br/>
+            <input className='activity' onChange={handleChange} value={activity} type="text" name="activity"/><br/>
             <label for="hours">Enter Hours</label>
-            <input type="text" name="hours"/><br/>
-            <button type="button" name="total">Submit</button>
+            <input className='hours' onChange={handleChange} value={hours} type="text" name="hours"/><br/>
+            <button onClick={addHealthStatManual} type="button" name="total">Submit</button>
             <div>
                 <h6>Totals:</h6>
                 <h6 className="totals"></h6>
